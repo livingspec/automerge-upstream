7e13962c8290f9de8accef3b8e99c506a9175007
Nemanja Tosic <nemanja@livingspec.com>
2021-05-01 16:45:27 +0200

    Add a failing unit test with profiling for re-hydrating text changes

---
+++ test/text_performance_test.js
@@ -0,0 +1,103 @@
+/* eslint-disable no-console */
+const {
+  performance,
+  PerformanceObserver,
+} = require("perf_hooks")
+const Automerge = require("../src/automerge")
+
+// Automerge.setDefaultBackend(require("../automerge-rs/automerge-backend-wasm/build/cjs"))
+
+describe("Rehydrating a document with a lot of text changes", () => {
+  let perfObserver
+
+  before(() => {
+    perfObserver = new PerformanceObserver((items) => {
+      console.log(
+        "change count".padEnd(15),
+        "type".padEnd(15),
+        "duration (ms)"
+      )
+      items.getEntries().forEach((entry) => {
+        console.log(
+          entry.detail.count.toString().padEnd(15),
+          entry.detail.type.toString().padEnd(15),
+          Math.round(entry.duration)
+        )
+      })
+    })
+
+    perfObserver.observe({
+      entryTypes: ["measure"],
+      buffered: true
+    })
+  })
+
+  after(() => perfObserver.disconnect())
+
+  // TOOD: 50ms is arbitrary. The event loop must not be blocked for very long though
+  // NOTE: since both applyChanges and snapshot are sync functions, there is no need to measure event loop utilization
+  const maxBlockingTimeMs = 50
+  const measuredChanges = [500, 1000, 2000, 5000]
+
+  context('applyChanges', () => {
+    measuredChanges.forEach((textChanges) => {
+      it(`should not block the event loop for more than ${maxBlockingTimeMs}ms`, function () {
+        this.timeout(maxBlockingTimeMs)
+
+        applyChanges(textChanges)
+      })
+    })
+  })
+
+  context('snapshot', () => {
+    measuredChanges.forEach((textChanges) => {
+      it(`should not block the event loop for more than ${maxBlockingTimeMs}ms`, function () {
+        this.timeout(maxBlockingTimeMs)
+
+        snapshot(textChanges)
+      })
+    })
+  })
+})
+
+function applyChanges(textChanges) {
+  let n1 = simulateInputOverTime(textChanges)
+
+  measureAround(() => Automerge.applyChanges(Automerge.init(), Automerge.getAllChanges(n1)), {
+    count: textChanges,
+    type: 'applyChanges'
+  })
+}
+
+function snapshot(textChanges) {
+  let snapshot = Automerge.save(simulateInputOverTime(textChanges))
+
+  measureAround(() => Automerge.load(snapshot), {
+    count: textChanges,
+    type: 'snapshot'
+  })
+}
+
+function simulateInputOverTime(textChanges) {
+  let doc = Automerge.init()
+
+  doc = Automerge.change(doc, { time: 0 }, (doc) => (doc.n = new Automerge.Frontend.Text()))
+  for (let i = 0; i < textChanges; i++) {
+    doc = Automerge.change(doc, { time: 0 }, (doc) => doc.n.insertAt(0, 'a'))
+  }
+
+  return doc
+}
+
+function measureAround(action, detail) {
+  const START_MARKER = `start`
+
+  performance.mark(START_MARKER)
+
+  action()
+
+  performance.measure(`sync`, {
+    detail,
+    start: START_MARKER,
+  })
+}
